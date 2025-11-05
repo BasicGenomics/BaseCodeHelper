6ea5cff1a02da1f04bf56145a5fa9beb53926af7
AntonJMLarsson <antonlarsson23@gmail.com>
2025-11-05 11:44:35 +0100

    Added Singularity support

--- BaseCodeHelper.sh
+++ BaseCodeHelper.sh
@@ -25,7 +25,8 @@ prompt_no_default a_dir "Enter path for results directory (host): "
 prompt_no_default b_dir "Enter path for configuration directory (host): " 
 prompt_no_default c_dir "Enter path for FASTQ directory (host): " 
 prompt_no_default d_dir "Enter path for resources directory (host): " 
-prompt_with_default num_threads "Enter number of threads" "$DEFAULT_NUM_THREADS"
+prompt_with_default num_threads "Enter number of threads (default 24)" "$DEFAULT_NUM_THREADS"
+prompt_with_default software "Specify deployment software (default docker)" "docker"
 
 # Display the inputs
 echo ""
@@ -35,6 +36,7 @@ echo "Path for configuration directory (host): $b_dir"
 echo "Path for FASTQ directory (host): $c_dir"
 echo "Path for resources directory (host): $d_dir"
 echo "Number of Threads: $num_threads"
+echo "Deployment software: $software"
 echo ""
 
 # Define Docker container paths
@@ -45,7 +47,7 @@ mnt_c="/usr/local/BaseCode/fastq/"
 
 # Construct the Docker command
 docker_cmd=(
-	docker 
+	$software
 	run 
 	--mount type=bind,src="$a_dir",dst="$mnt_a" 
 	--mount type=bind,src="$b_dir",dst="$mnt_b"  
@@ -56,7 +58,7 @@ docker_cmd=(
 )
 
 # Print the Docker command
-echo "Running the following Docker command:"
+echo "Running the following command:"
 printf '%q ' "${docker_cmd[@]}"
 echo ""
 echo ""
